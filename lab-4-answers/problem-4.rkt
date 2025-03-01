5aa1d70450b31501459764579813bbf6ff18f5b1
Jack Widman <jack.widman@gmail.com>
2025-02-28 19:02:01 -0500

    Renamed a variable

--- lab-4-answers/problem-4.rkt
+++ lab-4-answers/problem-4.rkt
@@ -1,26 +1,22 @@
 (require 2htdp/universe)
 (require 2htdp/image)
 
-(define-struct worldxy [x y])
+(define-struct pos [x y])
 (define BACKGROUND (empty-scene 1000 1000))
-(define INITIAL-STATE  (make-worldxy 100 100))
+(define INITIAL-STATE  (make-pos 100 100))
 
 (define (render-world state)
   (place-image (square 20 "solid" "black")
-               (worldxy-x state)
-               (worldxy-y state)
+               (pos-x state)
+               (pos-y state)
                BACKGROUND))
 
 (define (update-world state)
   (if
-     (< (worldxy-x state) 500)
-     (make-worldxy (+ 1 (worldxy-x state)) (worldxy-y state))
-     (make-worldxy (worldxy-x state) (+ 1 (worldxy-y state)))))
+     (< (pos-x state) 500)
+     (make-pos (+ 1 (pos-x state)) (pos-y state))
+     (make-pos (pos-x state) (+ 1 (pos-y state)))))
 
 (big-bang INITIAL-STATE
     [to-draw render-world]
     [on-tick update-world 0.001])  
-
- 
-
- 
